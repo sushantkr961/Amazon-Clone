367d2c53439cf2fef5a4c3de90912889d6a0207d
sushantkr961 <sushantonly961@gmail.com>
2023-03-07 00:20:44 +0530

    category filter and checkbox disabled part done

--- frontend/src/components/filterQueryResultOptions/CategoryFilterComponent.jsx
+++ frontend/src/components/filterQueryResultOptions/CategoryFilterComponent.jsx
@@ -1,13 +1,60 @@
+import { useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const CategoryFilterComponent = ({ setCategoriesFromFilter }) => {
   const { categories } = useSelector((state) => state.getCategories);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const myRefs = useRef([]);
+
+  // console.log(myRefs)
 
   const selectCategory = (e, category, idx) => {
     setCategoriesFromFilter((items) => {
       return { ...items, [category.name]: e.target.checked };
     });
+
+    var selectedMainCategory = category.name.split("/")[0];
+    // console.log(selectedMainCategory)
+    var allCategories = myRefs.current.map((_, id) => {
+      return { name: categories[id].name, idx: id };
+    });
+    // console.log(allCategories)
+    var indexesOfMainCategory = allCategories.reduce((acc, item) => {
+      var cat = item.name.split("/")[0];
+      if (selectedMainCategory === cat) {
+        acc.push(item.idx);
+      }
+      return acc;
+    }, []);
+    // console.log(indexesOfMainCategory)
+    if (e.target.checked) {
+      setSelectedCategories((old) => [...old, "cat"]);
+      myRefs.current.map((_, idx) => {
+        if (!indexesOfMainCategory.includes(idx)) {
+          myRefs.current[idx].disabled = true;
+        }
+        return "";
+      });
+    } else {
+      setSelectedCategories((old) => {
+        var a = [...old];
+        a.pop();
+        if (a.length === 0) {
+          window.location.href = "/product-list";
+        }
+        return a;
+      });
+      myRefs.current.map((_, idx2) => {
+        if (allCategories.length === 1) {
+          if (idx2 !== idx) {
+            myRefs.current[idx2].disabled = false;
+          }
+        } else if (selectedCategories.length === 1)
+          myRefs.current[idx2].disabled = false;
+        return "";
+      });
+    }
   };
 
   return (
@@ -21,6 +68,7 @@ const CategoryFilterComponent = ({ setCategoriesFromFilter }) => {
                 type="checkbox"
                 isValid
                 onChange={(e) => selectCategory(e, category, idx)}
+                ref={(el) => (myRefs.current[idx] = el)}
               />
               <Form.Check.Label style={{ cursor: "pointer" }}>
                 {category.name}
