0892c16ac2b11b70fee2c30e85a3ca92f27f9234
sushantkr961 <sushantonly961@gmail.com>
2023-03-07 02:03:30 +0530

    filter product done from api is made dynamically

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -4,10 +4,49 @@ import { useSelector } from "react-redux";
 
 let filtersUrl = ""
 
+const proceedFilters = (filters) => {
+  filtersUrl=""
+  Object.keys(filters).map((key,index) => {
+    if (key === "price") {
+      filtersUrl += `&price=${filters[key]}`
+    }else if (key === "rating"){
+      let rat = ""
+      Object.keys(filters[key]).map((key2, index2) => {
+        if (filters[key][key2]) {
+          rat += `${key2},`
+        }
+        return ""
+      })
+      filtersUrl += "&rating=" + rat
+    }else if (key === "category"){
+      let cat = ""
+      Object.keys(filters[key]).map((key3, index3) => {
+        if (filters[key][key3]) {
+          cat += `${key3},`
+        }
+        return ""
+      })
+      filtersUrl += "&category=" + cat
+    }else if (key === "attrs") {
+      if (filters[key].length > 0) {
+        let val = filters[key].reduce((acc,item) => {
+          let key = item.key;
+          let valu = item.values.join("-")
+          return acc+key+"-" + valu +","
+        }, "")
+        filtersUrl += "&attrs=" + val
+      }
+    }
+    return ""
+  })
+  return filtersUrl
+}
+
 const getProducts = async (filters={}, sortOption="", categoryName="", pageNumParam=null, searchQuery="") => {
   // filtersUrl = "&price=60&rating=1,2,3&category=a,b,c,d&attrs=color-red-blue,size-1TB-2TB"
-  filtersUrl = ""
-  console.log(filters)
+  filtersUrl = proceedFilters(filters)
+  // console.log(filters)
+  
 
   const search = searchQuery ? `search/${searchQuery}/`: ""
   const category = categoryName ? `category/${categoryName}/`: ""
