1cd4edc57a6678b95076068a6dcdf293f8ec8121
sushantkr961 <sushantonly961@gmail.com>
2023-03-07 13:51:53 +0530

    admin analytics page done

---
+++ frontend/src/pages/admin/components/AnalyticsPageComponent.jsx
@@ -0,0 +1,187 @@
+import { useEffect, useState } from "react";
+import { Row, Col, Form } from "react-bootstrap";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import AdminLinksComponents from "../../../components/admin/AdminLinksComponents";
+
+const AnalyticsPageComponent = ({
+  fetchOrdersForFirstDate,
+  fetchOrdersForSecondDate,
+}) => {
+  const [firstDateToCompare, setFirstDateToCompare] = useState(
+    new Date().toISOString().substring(0, 10)
+  );
+
+  var previousDay = new Date();
+  previousDay.setDate(previousDay.getDate() - 1);
+
+  const [secondDateToCompare, setSecondDateToCompare] = useState(
+    new Date(previousDay).toISOString().substring(0, 10)
+  );
+
+  const [dataForFirstSet, setDataForFirstSet] = useState([]);
+  const [dataForSecondSet, setDataForSecondSet] = useState([]);
+
+  useEffect(() => {
+    const abctrl = new AbortController();
+    fetchOrdersForFirstDate(abctrl, firstDateToCompare)
+      .then((data) => {
+        // console.log(data)
+        let orderSum = 0;
+        const orders = data.map((order) => {
+          orderSum += order.orderTotal.cartSubtotal;
+          var date = new Date(order.createdAt).toLocaleString("en-US", {
+            hour: "numeric",
+            hour12: true,
+            timeZone: "UTC",
+          });
+          return { name: date, [firstDateToCompare]: orderSum };
+        });
+        // console.log(orders)
+        setDataForFirstSet(orders);
+      })
+      .catch((er) =>
+        console.log(
+          er.response.data.message ? er.response.data.message : er.response.data
+        )
+      );
+
+    fetchOrdersForSecondDate(abctrl, secondDateToCompare)
+      .then((data) => {
+        // console.log(data)
+        let orderSum = 0;
+        const orders = data.map((order) => {
+          orderSum += order.orderTotal.cartSubtotal;
+          var date = new Date(order.createdAt).toLocaleString("en-US", {
+            hour: "numeric",
+            hour12: true,
+            timeZone: "UTC",
+          });
+          return { name: date, [secondDateToCompare]: orderSum };
+        });
+        // console.log(orders)
+        setDataForSecondSet(orders);
+      })
+      .catch((er) =>
+        console.log(
+          er.response.data.message ? er.response.data.message : er.response.data
+        )
+      );
+  }, [firstDateToCompare, secondDateToCompare]);
+
+  const firstDateHandler = (e) => {
+    setFirstDateToCompare(e.target.value);
+  };
+
+  const secondDateHandler = (e) => {
+    setSecondDateToCompare(e.target.value);
+  };
+
+  return (
+    <Row className="m-5">
+      <Col md={2}>
+        <AdminLinksComponents />
+      </Col>
+      <Col md={10}>
+        <h1>
+          Black Friday Cumulative Revenue {firstDateToCompare} VS{" "}
+          {secondDateToCompare}
+        </h1>
+        <Form.Group controlId="firstDateToCompare">
+          <Form.Label>Select First Date To Compare</Form.Label>
+          <Form.Control
+            type="date"
+            name="firstDateToCompare"
+            placeholder="First Date To Compare"
+            onChange={firstDateHandler}
+            defaultValue={firstDateToCompare}
+          />
+        </Form.Group>
+        <br />
+        <Form.Group controlId="secondDateToCompare">
+          <Form.Label>Select Second Date To Compare</Form.Label>
+          <Form.Control
+            type="date"
+            name="secondDateToCompare"
+            placeholder="Second Date To Compare"
+            onChange={secondDateHandler}
+            defaultValue={secondDateToCompare}
+          />
+        </Form.Group>
+        <ResponsiveContainer width="100%" height={500}>
+          <LineChart
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="name"
+              label={{
+                value: "TIME",
+                offset: 50,
+                position: "insideBottomRight",
+              }}
+              allowDuplicatedCategory={false}
+            />
+            <YAxis
+              label={{ value: "REVENUE $", angle: -90, position: "insideLeft" }}
+            />
+            <Tooltip />
+            <Legend verticalAlign="top" height={36} />
+            {dataForFirstSet.length > dataForSecondSet.length ? (
+              <>
+                <Line
+                  data={dataForFirstSet}
+                  type="monotone"
+                  dataKey={firstDateToCompare}
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                  strokeWidth={4}
+                />
+                <Line
+                  data={dataForSecondSet}
+                  type="monotone"
+                  dataKey={secondDateToCompare}
+                  stroke="#82ca9d"
+                  strokeWidth={4}
+                />
+              </>
+            ) : (
+              <>
+                <Line
+                  data={dataForSecondSet}
+                  type="monotone"
+                  dataKey={secondDateToCompare}
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                  strokeWidth={4}
+                />
+                <Line
+                  data={dataForFirstSet}
+                  type="monotone"
+                  dataKey={firstDateToCompare}
+                  stroke="#82ca9d"
+                  strokeWidth={4}
+                />
+              </>
+            )}
+          </LineChart>
+        </ResponsiveContainer>
+      </Col>
+    </Row>
+  );
+};
+
+export default AnalyticsPageComponent;
